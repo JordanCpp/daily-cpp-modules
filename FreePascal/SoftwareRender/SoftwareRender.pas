19d172097651620415aa0555214420e09bd20cf9
JordanCpp <Jordan631@yandex.ru>
2026-08-10 14:06:31 +0400

    Update license

--- FreePascal/SoftwareRender/SoftwareRender.pas
+++ FreePascal/SoftwareRender/SoftwareRender.pas
@@ -1,3 +1,10 @@
+{
+  Copyright 2026-present Evgeny Zoshchuk (JordanCpp).
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or copy at
+  https://boost.org)
+}
+
 unit SoftwareRender;
 
 {$mode ObjFPC}{$H+}
